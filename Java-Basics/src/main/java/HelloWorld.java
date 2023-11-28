31f7d14066093be408cc04cc07d299ce457ce669
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2023-11-28 13:44:06 +0200

    declaration of variables

--- Java-Basics/src/main/java/HelloWorld.java
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 public class HelloWorld {
+    public static Scanner s = new Scanner(System.in);
     static {
         System.out.println("initialization is here ");
     }
     public static void main(String[] args) {
         System.out.print("hello, world");
-        System.out.print(" hi");
+        System.out.println(" hi");
+        System.out.print("Enter the first number : ");
+        int x = s.nextInt();
+        System.out.print("Enter the second number : ");
+        int y = s.nextInt();
+        System.out.println("the Result is : "+(x+y));
     }
 }
