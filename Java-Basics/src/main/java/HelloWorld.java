8653c1d8ee9f3a6e56e5a954ca0c23e9e49570dd
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2023-11-28 13:20:05 +0200

    using print method instead of println and \n

--- Java-Basics/src/main/java/HelloWorld.java
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
         System.out.println("initialization is here ");
     }
     public static void main(String[] args) {
-        System.out.println("hello, world");
+        System.out.print("hello, world");
+        System.out.print(" hi");
     }
 }
