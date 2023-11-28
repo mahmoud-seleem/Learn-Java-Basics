800d988a29139e885e36345648ee0563faa6c90a
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2023-11-28 11:43:37 +0200

    saying hello world in java

---
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    static {
+        System.out.println("initialization is here ");
+    }
+    public static void main(String[] args) {
+        System.out.println("hello, world");
+    }
+}
