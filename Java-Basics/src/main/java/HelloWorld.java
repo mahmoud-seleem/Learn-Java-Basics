a941ff85071252f5357f7abd90d9c34229eb797e
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2023-12-01 18:11:10 +0200

    exploring the control statement if-else

--- Java-Basics/src/main/java/HelloWorld.java
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -5,14 +5,11 @@ public class HelloWorld {
         System.out.println("initialization is here ");
     }
     public static void main(String[] args) {
-//        System.out.print("hello, world");
-//        System.out.println(" hi");
-//        System.out.print("Enter the first number : ");
-//        int x = s.nextInt();
-//        System.out.print("Enter the second number : ");
-//        int y = s.nextInt();
-//        System.out.println("the Result is : "+(x+y));
-        String s = "\uD83E\uDD70";
-        System.out.println(s);
+        boolean x = false;
+        if (x){
+            System.out.println("TRUE");
+        }else {
+            System.out.println("FALSE");
+        }
     }
 }
