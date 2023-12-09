6a46d6e1ff8c0fcd32729dbbd1825f7b70434810
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2023-12-09 15:30:41 +0200

    static nested interface inside a class

---
+++ Java-Basics/src/main/java/OOP/polymorphism/Nested.java
@@ -0,0 +1,15 @@
+package OOP.polymorphism;
+
+public class Nested {
+    static int x ;
+    interface NestedInterface{
+        int z = x;
+        private static void prr(){
+            pr();
+        }
+    }
+
+    private static void pr(){
+
+    }
+}
