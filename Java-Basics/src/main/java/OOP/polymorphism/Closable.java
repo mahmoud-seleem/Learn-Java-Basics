3f68a4b163dec3bdbc26dafd3117e0601033396f
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2023-12-09 16:41:55 +0200

    static import instead of implementing interface of constants

--- Java-Basics/src/main/java/OOP/polymorphism/Closable.java
+++ Java-Basics/src/main/java/OOP/polymorphism/Closable.java
@@ -3,9 +3,10 @@ package OOP.polymorphism;
 public interface Closable {
     void close();
     int z  = 10;
-    interface C{
-        default void print(){
-            System.out.println(z);
-        }
+
+}
+class xx implements Closable{
+    public void close(){
+        System.out.println(z);
     }
 }
