a1faa1ba3c45bcf19e5d6f6569816d17a0253191
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2023-12-09 15:36:05 +0200

    nested interface inside another interface is implicitly public and static

--- Java-Basics/src/main/java/OOP/polymorphism/Closable.java
+++ Java-Basics/src/main/java/OOP/polymorphism/Closable.java
@@ -2,4 +2,10 @@ package OOP.polymorphism;
 
 public interface Closable {
     void close();
+    int z  = 10;
+    interface C{
+        default void print(){
+            System.out.println(z);
+        }
+    }
 }
