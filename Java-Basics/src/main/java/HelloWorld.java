c4871aece4029c26bdd0cabf637007bc095e3303
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2023-12-11 17:45:56 +0200

    using wildcards in java generics

--- Java-Basics/src/main/java/HelloWorld.java
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -5,23 +5,27 @@ import OOP.inheritance.Human;
 import OOP.inheritance.Person;
 import OOP.polymorphism.Anonymous;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 public class HelloWorld {
     static {
         System.out.println("initialization of HelloWorld is here ");
     }
     public static void main(String[] args) {
-        Generic<Integer> p = new Generic<>(5);
-        Generic pp = new Generic();
-        System.out.println(pp.gene);
-//        p.actualName(0.5);
+       print(Arrays.asList(1,2,3,4));
     }
-    public static <T extends Comparable<T>> int countGreaterThan(T[] anArray, T elem) {
-        int count = 0;
-        for (T e : anArray)
-            if (e.compareTo(elem) > 0)
-                ++count;
-        return count;
+
+    public static <E> void print(List<E> list){
+        for (E element : list){
+            System.out.println(element.getClass().getName());
+        }
+    }
+    public static  void print(List<?> list){
+        for (Object element : list){
+            System.out.println(element.getClass().getName());
+        }
     }
 
 
--- Java-Basics/src/main/java/OOP/generics/Generic.java
+++ Java-Basics/src/main/java/OOP/generics/Generic.java
@@ -6,11 +6,10 @@ import OOP.polymorphism.Playable;
 public class Generic<E extends Number> {
     public E gene ;
 
-    public Generic(E gene){
-        this.gene = gene;
+    public <S> Generic(S gene){
     }
     public Generic(){}
-    public <N extends Number> void actualName(N gene){
-        System.out.println(gene.doubleValue());
+    public <N> void actualName(N gene){
+        System.out.println(gene);
     }
 }
