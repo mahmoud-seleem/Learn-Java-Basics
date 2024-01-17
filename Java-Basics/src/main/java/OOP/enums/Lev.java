14bfa3b894b0a83c1acfd017d6f7e9cf1edfb209
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-01-17 14:45:21 +0300

    building a real enum

---
+++ Java-Basics/src/main/java/OOP/enums/Lev.java
@@ -0,0 +1,32 @@
+package OOP.enums;
+
+public enum Lev  {
+    HIGH(10){
+        @Override
+        public void print() {
+
+        }
+    },MEDIUM(5){
+        @Override
+        public void print() {
+
+        }
+    },LOW(1){
+        @Override
+        public void print() {
+
+        }
+    };
+
+    private int levelNumber;
+    Lev(int levelNumber){
+        this.levelNumber = levelNumber;
+    }
+    public void setLevelNumber(int levelNumber){
+        this.levelNumber = levelNumber;
+    }
+    public int getLevelNumber(){
+        return this.levelNumber;
+    }
+    public abstract void print();
+}
