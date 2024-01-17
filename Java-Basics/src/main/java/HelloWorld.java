a38835276cd70d239b45d15cf883b881e50e56f6
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-01-17 14:07:19 +0300

    making constants with classes instead of regular constants is better but can't be used in the switch statement

--- Java-Basics/src/main/java/HelloWorld.java
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -1,3 +1,4 @@
+import OOP.enums.Level;
 import OOP.generics.*;
 
 import java.util.ArrayList;
@@ -10,16 +11,7 @@ public class HelloWorld {
         System.out.println("initialization of HelloWorld is here ");
     }
     public static void main(String[] args) {
-        Box<Integer> box = new Box<>();
-        box.setElement(5);
-        int x = box.getElement();
-        Box b = box;
-        box = b;
-
-    }
-    public void ss(List<? extends Number & Comparator<Number>){}
-    public <C> void s(C[] cs){
-        System.out.println(cs.length);
+        Level level = Level.HIGH;
     }
 }
 
