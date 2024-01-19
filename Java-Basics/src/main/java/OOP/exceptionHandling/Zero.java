d705a765602d91c357ec564a8de5d7fbb222f339
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-01-19 22:25:57 +0300

    finally block thrown excepitions make the prev ones lost

--- Java-Basics/src/main/java/OOP/exceptionHandling/Zero.java
+++ Java-Basics/src/main/java/OOP/exceptionHandling/Zero.java
@@ -4,15 +4,21 @@ import java.util.InputMismatchException;
 
 public class Zero {
     public static void main(String[] args) throws Exception {
-        try{try{
-            div(10,0);
-        }catch (ArithmeticException e ) {
-            System.out.println("hi i'm the exception");
-            throw new Exception("hi");
+//        try{
+//            div(10,10);
+//        }catch (ArithmeticException e ) {
+//            System.out.println("hi i'm the exception");
+//            throw e;
+//        }
+        catchTheException(10,0);
+    }
+    public static void catchTheException(int x , int y){
+        try {
+            div(x,y);
+        }catch (Exception e){
+            throw new Exception("new exception thrown from the catch in catch method");
         }finally {
-            System.out.println("hello from the finally block ");
-        }}catch (Exception e){
-            System.out.println("handling the exception");
+            throw new RuntimeException("new exception in the finally block");
         }
     }
     public static int div(int q1,int q2){
