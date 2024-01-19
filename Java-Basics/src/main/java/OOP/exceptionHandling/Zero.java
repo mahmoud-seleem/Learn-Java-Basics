fe06d4e7682b324a9a2e16fc38dd549f29af7eef
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-01-19 14:36:45 +0300

    examining the finally block and when will it executes

--- Java-Basics/src/main/java/OOP/exceptionHandling/Zero.java
+++ Java-Basics/src/main/java/OOP/exceptionHandling/Zero.java
@@ -3,13 +3,16 @@ package OOP.exceptionHandling;
 import java.util.InputMismatchException;
 
 public class Zero {
-    public static void main(String[] args) {
-        try{
+    public static void main(String[] args) throws Exception {
+        try{try{
             div(10,0);
-        }catch (ArithmeticException e ){
-            System.out.println(e.getClass());
-        }catch (Exception e ){
-            System.out.println(e.getMessage());
+        }catch (ArithmeticException e ) {
+            System.out.println("hi i'm the exception");
+            throw new Exception("hi");
+        }finally {
+            System.out.println("hello from the finally block ");
+        }}catch (Exception e){
+            System.out.println("handling the exception");
         }
     }
     public static int div(int q1,int q2){
