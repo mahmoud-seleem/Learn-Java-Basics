d4c713c27402d2cd76ed45b9c358942f633b5ec8
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-01-22 22:53:45 +0300

    playing with nested lambda expressions

--- Java-Basics/src/main/java/HelloWorld.java
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -1,11 +1,28 @@
 import java.util.Scanner;
+import java.util.concurrent.Callable;
 
 public class HelloWorld {
 
 
     public static void main(String[] args) {
-        Thread t = new Thread(() -> System.out.println("hello"));
+        System.out.println(calculate((double val1,double val2) -> val1 + val2 ,15,20));
+        
     }
+    static double calculate(BinaryCalculator calc, double v1, double v2)
+    {
+        return calc.calculate(v1, v2);
+    }
+    static double calculate(UnaryCalculator calc, double v)
+    {
+        return calc.calculate(v);
+    }
+}
+interface BinaryCalculator{
+    double calculate(double val1,double val2);
+}
+interface UnaryCalculator
+{
+    double calculate(double value);
 }
 
 
