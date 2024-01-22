5a3e69aa65009845f65b60ac37be3973a3061c6d
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-01-22 21:06:46 +0300

    before lambda we use the anonymous classes

--- Java-Basics/src/main/java/HelloWorld.java
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -1,21 +1,15 @@
 import java.util.Scanner;
 
 public class HelloWorld {
-    static {
-        System.err.println("initialization of HelloWorld is here ");
-    }
+
 
     public static void main(String[] args) {
-        for (int i = 32; i < 127; i++) {
-            System.out.write(i);
-// break line after every eight characters.
-            
-            if (i % 8 == 7)
-                System.out.write('\n');
-            else
-                System.out.write('\t');
-        }
-        System.out.write('\n');
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("hello");
+            }
+        });
     }
 }
 
