9d6a5d48a2f019724cbc6bdd48d8ba4fc89e34c6
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-01-23 23:51:41 +0300

    exploring the built in annotations

--- Java-Basics/src/main/java/OOP/Demo.java
+++ Java-Basics/src/main/java/OOP/Demo.java
@@ -1,4 +1,19 @@
 package OOP;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(value = {ElementType.ANNOTATION_TYPE,ElementType.METHOD})
+@Demo
+
 public @interface Demo {
+}
+
+
+class DD{
+
+    @Demo
+     void mm(){
+
+    }
 }
