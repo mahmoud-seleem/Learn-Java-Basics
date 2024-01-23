9e4ff5e5d70c25a2b1964b3b2e53a8cf9888a21d
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-01-23 15:09:28 +0300

    method references

--- Java-Basics/src/main/java/HelloWorld.java
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -1,18 +1,22 @@
 import OOP.lambda.Child;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.util.concurrent.Callable;
 import java.util.function.Predicate;
 
 public class HelloWorld {
 
-    BinaryCalculator b = ((val1, val2) -> val1 *val2);
-
     public static void main(String[] args) {
-//        System.out.println(calculate((double val1,double val2) -> val1 + val2 ,15,20));
-        Child child = new Child();
-        child.l();
-        BinaryCalculator b = ((val1, val2) -> val1 *val2);
+        Printable p = new Printable() {
+            @Override
+            public void print(String s) {
+                System.out.println("override"+ s);
+
+            }
+        };
+        p.print("");
+
     }
     static double calculate(BinaryCalculator calc, double v1, double v2)
     {
@@ -29,6 +33,11 @@ interface BinaryCalculator{
 interface UnaryCalculator
 {
     double calculate(double value);
+}
+interface Printable{
+    default void  print(String s){
+        System.out.println("default");
+    };
 }
 
 
