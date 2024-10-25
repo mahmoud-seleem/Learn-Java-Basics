22aaf26154c9bffd7e4b600266164ac9fb6441fc
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-10-25 11:51:33 +0300

    cleaning some code to start revision of java basics.

--- Java-Basics/src/main/java/HelloWorld.java
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -8,37 +8,30 @@ import java.util.function.Predicate;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        Printable p = new Printable() {
-            @Override
-            public void print(String s) {
-                System.out.println("override"+ s);
-
-            }
-        };
-        p.print("");
 
     }
-    static double calculate(BinaryCalculator calc, double v1, double v2)
-    {
-        return calc.calculate(v1, v2);
-    }
-    static double calculate(UnaryCalculator calc, double v)
-    {
-        return calc.calculate(v);
-    }
 }
-interface BinaryCalculator{
-    double calculate(double val1,double val2);
-}
-interface UnaryCalculator
-{
-    double calculate(double value);
-}
-interface Printable{
-    default void  print(String s){
-        System.out.println("default");
-    };
-}
+//    static double calculate(BinaryCalculator calc, double v1, double v2)
+//    {
+//        return calc.calculate(v1, v2);
+//    }
+//    static double calculate(UnaryCalculator calc, double v)
+//    {
+//        return calc.calculate(v);
+//    }
+//}
+//interface BinaryCalculator{
+//    double calculate(double val1,double val2);
+//}
+//interface UnaryCalculator
+//{
+//    double calculate(double value);
+//}
+//interface Printable{
+//    default void  print(String s){
+//        System.out.println("default");
+//    };
+//}
 
 
 
