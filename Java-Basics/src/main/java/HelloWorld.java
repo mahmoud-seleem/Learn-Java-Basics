e035bfbc5b3d63cdfc8b7c034fdf1fe47faf8072
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-10-25 14:51:53 +0300

    chapter 2 basics finished

--- Java-Basics/src/main/java/HelloWorld.java
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.printf("hello%n%s","world!");
+        Scanner input =  new Scanner(System.in);
+        System.out.print("enter the first number to add : ");
+        float x = input.nextFloat();
+        System.out.print("enter the second number to add : ");
+        float y = input.nextFloat();
+        System.out.printf("The sum is : %f" , x%y);
     }
 }
