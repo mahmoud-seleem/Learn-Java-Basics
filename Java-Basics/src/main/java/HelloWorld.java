37b92c7cbe17591a5b8b837829022bead61cd708
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-10-25 12:37:26 +0300

    cleaning some code to start revision of java basics.

--- Java-Basics/src/main/java/HelloWorld.java
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -1,37 +1,7 @@
-import OOP.lambda.Child;
-
-import java.sql.SQLOutput;
-import java.util.Scanner;
-import java.util.concurrent.Callable;
-import java.util.function.Predicate;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
-
+        System.out.printf("hello%n%s","world!");
     }
 }
-//    static double calculate(BinaryCalculator calc, double v1, double v2)
-//    {
-//        return calc.calculate(v1, v2);
-//    }
-//    static double calculate(UnaryCalculator calc, double v)
-//    {
-//        return calc.calculate(v);
-//    }
-//}
-//interface BinaryCalculator{
-//    double calculate(double val1,double val2);
-//}
-//interface UnaryCalculator
-//{
-//    double calculate(double value);
-//}
-//interface Printable{
-//    default void  print(String s){
-//        System.out.println("default");
-//    };
-//}
-
-
-
