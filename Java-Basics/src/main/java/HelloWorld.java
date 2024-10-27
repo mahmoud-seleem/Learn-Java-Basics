d01805939fed3d1c06cf360126cbbeec10340f1f
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-10-27 15:45:33 +0300

    exploring the switch statement:

--- Java-Basics/src/main/java/HelloWorld.java
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -4,14 +4,23 @@ import java.util.Scanner;
 public class HelloWorld {
 
     public static void main(String[] args) throws InterruptedException {
-        Scanner input =  new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         int counter = 10;
-        int x = input.nextInt();
-        do{
-            System.out.println(counter);
-            x++;
+        int x;
+        while (true) {
+            System.out.println("Enter number");
+            x = input.nextInt();
+            switch (x) {
+                case 1:
+                    System.out.println(1);
+                    break;
+                default:
+                    System.out.println("Default");
+                case 2:
+                    System.out.println(2);
+                    break;
+            }
         }
-        while (x < counter);
     }
 }
 
