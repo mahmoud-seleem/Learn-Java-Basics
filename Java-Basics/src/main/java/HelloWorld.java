09d72374832c4ffe02e2ca4c9dca8b79c0666fef
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-10-27 11:50:53 +0300

    revision of control statements

--- Java-Basics/src/main/java/HelloWorld.java
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -4,10 +4,8 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         Scanner input =  new Scanner(System.in);
-        System.out.print("enter the first number to add : ");
-        float x = input.nextFloat();
-        System.out.print("enter the second number to add : ");
-        float y = input.nextFloat();
-        System.out.printf("The sum is : %f" , x%y);
+        if (input.nextBoolean()){
+            System.out.println("Hello World");
+        }
     }
 }
