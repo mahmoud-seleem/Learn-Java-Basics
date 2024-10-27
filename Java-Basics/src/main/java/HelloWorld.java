0077cf44c999ea98a4ffc95d5730e1bd9def97f9
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-10-27 12:49:34 +0300

    game of gussing the numbers

--- Java-Basics/src/main/java/HelloWorld.java
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -4,8 +4,12 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         Scanner input =  new Scanner(System.in);
-        if (input.nextBoolean()){
-            System.out.println("Hello World");
+        int counter = 10;
+        int x = 0;
+        while(x < counter){
+            System.out.print("Enter an integer: ");
+            x = input.nextInt();
         }
     }
 }
+
