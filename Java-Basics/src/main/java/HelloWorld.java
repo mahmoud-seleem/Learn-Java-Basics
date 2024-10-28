276d40df8f5240a3d245200a9e92c3df6605fdca
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-10-28 19:07:28 +0300

    exploring the package access with protected finalize method

--- Java-Basics/src/main/java/HelloWorld.java
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -1,8 +1,17 @@
+import out.Outer;
+
 import java.util.Objects;
 import java.util.Scanner;
 
 public class HelloWorld {
 
+    private String name;
+    public void setName(String name) throws Throwable {
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         printArray2(1,2,3);
@@ -17,6 +26,7 @@ public class HelloWorld {
             System.out.println(x);
         }
     }
+
 
 }
 
