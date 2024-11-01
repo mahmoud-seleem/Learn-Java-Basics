f08eb7dd3de0f3b467193a05180e5c69604a52d6
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-11-01 10:30:15 +0200

    exploring streams 2

--- Java-Basics/src/main/java/OOP/lambda/stream.java
+++ Java-Basics/src/main/java/OOP/lambda/stream.java
@@ -1,19 +1,27 @@
 package OOP.lambda;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.IntConsumer;
+import java.util.function.IntPredicate;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class stream {
     public static void main(String[] args) {
         int[] values = {3, 10, 6, 1, 4, 8, 2, 5, 9, 7};
+        String[] strings = {"Ahi","a","me"};
         // display original values
         System.out.print("Original values: ");
-        IntStream
-                .of(values)
-                .forEach(value -> System.out.println(value));
-        int sum = IntStream.
-                range(1,10).reduce(0,(x, y) -> x+y);
-        System.out.println(sum);
+        List<String> s = Arrays.stream(strings)
+                .filter(value -> value.compareTo("") > 0 )
+                .sorted(String.CASE_INSENSITIVE_ORDER.reversed())
+                .collect(Collectors.toList());
+//        int sum = IntStream.
+//                rangeClosed(1,10).sum();
+        System.out.println(s);
+
     }
 }
