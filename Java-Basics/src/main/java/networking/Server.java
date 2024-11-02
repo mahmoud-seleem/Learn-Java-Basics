0fcf039deb2d803f6281ca149ba5a1458df8ba81
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-11-02 21:22:31 +0200

    building server app with java

---
+++ Java-Basics/src/main/java/networking/Server.java
@@ -0,0 +1,32 @@
+package networking;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("Starting server...");
+        try (ServerSocket serverSocket = new ServerSocket(12345)){
+            while (true){
+                Socket connectionSocket = serverSocket.accept();
+                OutputStream outputStream = connectionSocket.getOutputStream();
+                System.out.println("Client connected");
+//                PrintWriter writer = new PrintWriter(outputStream, true);
+//                writer.println("Hello from Java Server!");
+                outputStream.write(5);
+                outputStream.write(6);
+                //outputStream.flush();
+//                Thread.sleep(5000l);
+                connectionSocket.close();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
