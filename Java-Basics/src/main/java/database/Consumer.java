11559ce4f4fbebad974f612d85d7dd5e80f62b84
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-11-02 16:00:36 +0200

    using resultset

--- Java-Basics/src/main/java/database/Consumer.java
+++ Java-Basics/src/main/java/database/Consumer.java
@@ -11,8 +11,22 @@ public class Consumer {
         final String SELECT_QUERY = "SELECT * FROM \"Student\"";
     try(Connection connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD)) {
         System.out.println("Connected to database");
-        Statement statement = connection.createStatement();
+        Statement statement = connection.createStatement(
+                ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE
+        );
         ResultSet result = statement.executeQuery(SELECT_QUERY);
+        ResultSetMetaData  metaData = result.getMetaData();
+        int columnCount = metaData.getColumnCount();
+        for (int i = 1; i <= columnCount; i++) {
+            System.out.print(metaData.getColumnName(i) + "\t");
+        }
+        System.out.println();
+        while (result.next()) {
+            for (int i = 1; i <= columnCount; i++) {
+                System.out.print(result.getString(i) + "\t");
+            }
+            System.out.println();
+        }
     }
     catch (SQLException e){
         System.out.println("Connection failed");
