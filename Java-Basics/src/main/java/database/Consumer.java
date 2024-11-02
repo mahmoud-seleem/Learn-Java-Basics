b8595aface50a30f2a53f15b23bc82624d4e4ee9
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-11-02 16:42:12 +0200

    jdbc postgresql driver does not support the type scroll sensitive option of the resultSet

--- Java-Basics/src/main/java/database/Consumer.java
+++ Java-Basics/src/main/java/database/Consumer.java
@@ -10,43 +10,51 @@ public class Consumer {
         final String PASSWORD = "postgres";
         final String SELECT_QUERY = "SELECT * FROM \"Student\" order by \"ID\"";
     try(Connection connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD)) {
+        DatabaseMetaData metaData0 = connection.getMetaData();
+        boolean scrollSensitiveUpdatable = metaData0.supportsResultSetType(ResultSet.TYPE_SCROLL_SENSITIVE) &&
+                metaData0.supportsResultSetConcurrency(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+        boolean scrollSensitiveReadOnly = metaData0.supportsResultSetType(ResultSet.TYPE_SCROLL_SENSITIVE) &&
+                metaData0.supportsResultSetConcurrency(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
         System.out.println("Connected to database");
-        connection.setAutoCommit(false);
-        Statement statement = connection.createStatement(
-                ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE
-        );
-        ResultSet result = statement.executeQuery(SELECT_QUERY);
-        ResultSetMetaData  metaData = result.getMetaData();
-        int columnCount = metaData.getColumnCount();
-        for (int i = 1; i <= columnCount; i++) {
-            System.out.print(metaData.getColumnName(i) + "\t");
-        }
-        System.out.println();
-        while (result.next()) {
-            for (int i = 1; i <= columnCount; i++) {
-                System.out.print(result.getString(i) + "\t");
-            }
-            System.out.println();
-        }
-        System.out.println("here update begins");
-        result.absolute(1);
-        result.updateString("Name","mohamed");
-        result.updateRow();
-        result.absolute(0);
-        connection.commit();
-        connection.setAutoCommit(true);
-        //result = statement.executeQuery(SELECT_QUERY);
-        System.out.println(result.getMetaData().getColumnCount());
-        for (int i = 1; i <= columnCount; i++) {
-            System.out.print(metaData.getColumnName(i) + "\t");
-        }
-        System.out.println();
-        while (result.next()) {
-            for (int i = 1; i <= columnCount; i++) {
-                System.out.print(result.getString(i) + "\t");
-            }
-            System.out.println();
-        }
+        System.out.println(scrollSensitiveUpdatable);
+        System.out.println(scrollSensitiveReadOnly);
+        return;
+//        connection.setAutoCommit(false);
+//        Statement statement = connection.createStatement(
+//                ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE
+//        );
+//        ResultSet result = statement.executeQuery(SELECT_QUERY);
+//        ResultSetMetaData  metaData = result.getMetaData();
+//        int columnCount = metaData.getColumnCount();
+//        for (int i = 1; i <= columnCount; i++) {
+//            System.out.print(metaData.getColumnName(i) + "\t");
+//        }
+//        System.out.println();
+//        while (result.next()) {
+//            for (int i = 1; i <= columnCount; i++) {
+//                System.out.print(result.getString(i) + "\t");
+//            }
+//            System.out.println();
+//        }
+//        System.out.println("here update begins");
+//        result.absolute(1);
+//        result.updateString("Name","mohamed");
+//        result.updateRow();
+//        result.absolute(0);
+//        connection.commit();
+//        connection.setAutoCommit(true);
+//        //result = statement.executeQuery(SELECT_QUERY);
+//        System.out.println(result.getMetaData().getColumnCount());
+//        for (int i = 1; i <= columnCount; i++) {
+//            System.out.print(metaData.getColumnName(i) + "\t");
+//        }
+//        System.out.println();
+//        while (result.next()) {
+//            for (int i = 1; i <= columnCount; i++) {
+//                System.out.print(result.getString(i) + "\t");
+//            }
+//            System.out.println();
+//        }
     }
     catch (SQLException e){
         System.out.println("Connection failed");
