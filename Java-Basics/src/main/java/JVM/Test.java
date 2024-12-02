6fc242d692da80dc1b011e09dd6e559cbad4f75c
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-12-02 18:40:42 +0200

    practice

--- Java-Basics/src/main/java/JVM/Test.java
+++ Java-Basics/src/main/java/JVM/Test.java
@@ -2,10 +2,9 @@ package JVM;
 
 public class Test {
     public static void main(String[] args) {
-        MyClass[] s = new MyClass[10];
-        MyClassParent[] a;
-        a = s;
-        a[0] = new AnotherChild("");
+        for (int x : new int[5]){
+
+        }
     }
 }
 
