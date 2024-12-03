c293b1638632a4310391fe7c8bbe01f63e44edf3
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-12-03 13:18:24 +0200

    practicing

--- Java-Basics/src/main/java/JVM/Test.java
+++ Java-Basics/src/main/java/JVM/Test.java
@@ -2,8 +2,19 @@ package JVM;
 
 public class Test {
     public static void main(String[] args) {
-        for (int x : new int[5]){
-
+       String x = "8";
+        switch(x){
+            case "5":
+            System.out.println("hi");
+            case "8" : 
+            System.out.println("hello");
+            break;
+            default:
+            System.out.println("default");
+            break;
+            case "58":
+            System.out.println("6");
+            break;
         }
     }
 }
