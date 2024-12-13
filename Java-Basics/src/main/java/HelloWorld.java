81efddea99ef2b7434a613f70ac33ea797627493
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-12-13 18:03:46 +0200

    java always create the args array and it's never a null even if no command line args are passed :

--- Java-Basics/src/main/java/HelloWorld.java
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -1,21 +1,7 @@
-import OOP.serialization.Acc;
-import out.Outer;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Formatter;
-import java.util.Objects;
-import java.util.Scanner;
-
 public class HelloWorld {
-
+    public static String name = "hello";
     public static void main(String[] args) {
-
+        
     }
 }
 
