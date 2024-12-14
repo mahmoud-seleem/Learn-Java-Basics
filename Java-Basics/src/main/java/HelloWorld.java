7074dbd7f88342f3d2ea7d76bf88fa96d0a9e005
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-12-14 20:25:48 +0200

    using labeled break and continue statements

--- Java-Basics/src/main/java/HelloWorld.java
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -4,18 +4,15 @@ public class HelloWorld implements Closable{
     public static String name = "hello";
     public static void main(String[] args) {
         int x = 0;
-        switch(x){
-            case 5:
-            case 4:
-            x = 5*x;
-            break;
-            default: 
-           break;
-            case 6:
-            x = 6*x;
-            break;
-            case 36:
-            break;
+        outer:
+        while (true) {
+            inner:
+            System.out.println("outer");
+            while (true) {
+                if (x == 1){
+                    break ;
+                }                     
+            }
         }
     }  
 
