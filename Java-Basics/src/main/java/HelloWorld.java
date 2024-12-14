5ff226d755dd7a315473fcec2bcf0ee944c85db2
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-12-14 13:47:55 +0200

    java make logic ops with int only

--- Java-Basics/src/main/java/HelloWorld.java
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -3,10 +3,9 @@ import OOP.polymorphism.Closable;
 public class HelloWorld implements Closable{
     public static String name = "hello";
     public static void main(String[] args) {
-        int عدد = 5; // Arabic letters
-        double π = 3.14; // Greek letter
-        String 你好 = "Hello"; // Chinese characters
-        System.out.println(π);
+      char c = 0x0111;
+      char x = 0x0011;
+      char z = (char) (c | x);
     }  
 
     public void close(){
