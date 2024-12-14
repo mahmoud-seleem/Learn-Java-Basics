4cdeb5ead64566afb8a302552a524e2332a845b2
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-12-14 11:07:14 +0200

    java check for the is a relation in interfaces references using the compiler and if passed it doesn't check at runtime when assign a subclass to a super ref

--- Java-Basics/src/main/java/HelloWorld.java
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -1,7 +1,13 @@
-public class HelloWorld {
+import OOP.polymorphism.Closable;
+
+public class HelloWorld implements Closable{
     public static String name = "hello";
     public static void main(String[] args) {
-        
+        Closable closable = new HelloWorld();
+    }
+
+    public void close(){
+
     }
 }
 
