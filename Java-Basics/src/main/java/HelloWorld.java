6bdd9f42f89422af24fcba460cb366059c626a51
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-12-14 13:22:10 +0200

    we can name identifiers with unicode chars

--- Java-Basics/src/main/java/HelloWorld.java
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -3,9 +3,11 @@ import OOP.polymorphism.Closable;
 public class HelloWorld implements Closable{
     public static String name = "hello";
     public static void main(String[] args) {
-       double x = 0.1f;
-       System.out.println(x);   
-    }
+        int عدد = 5; // Arabic letters
+        double π = 3.14; // Greek letter
+        String 你好 = "Hello"; // Chinese characters
+        System.out.println(π);
+    }  
 
     public void close(){
 
