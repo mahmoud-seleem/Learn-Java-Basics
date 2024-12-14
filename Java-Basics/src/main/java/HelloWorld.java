d0cf9870517261a862a917560be5459bb2972e93
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-12-14 19:24:39 +0200

    switch statement write the statements in the byte code in the same order they were written in the source code

--- Java-Basics/src/main/java/HelloWorld.java
+++ Java-Basics/src/main/java/HelloWorld.java
@@ -3,9 +3,20 @@ import OOP.polymorphism.Closable;
 public class HelloWorld implements Closable{
     public static String name = "hello";
     public static void main(String[] args) {
-      char c = 0x0111;
-      char x = 0x0011;
-      char z = (char) (c | x);
+        int x = 0;
+        switch(x){
+            case 5:
+            case 4:
+            x = 5*x;
+            break;
+            default: 
+           break;
+            case 6:
+            x = 6*x;
+            break;
+            case 36:
+            break;
+        }
     }  
 
     public void close(){
