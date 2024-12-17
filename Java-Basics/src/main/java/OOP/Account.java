0e01827d52afd086011476cad4502045f985830a
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-12-17 18:15:53 +0200

    annonymous class

--- Java-Basics/src/main/java/OOP/Account.java
+++ Java-Basics/src/main/java/OOP/Account.java
@@ -1,38 +1,44 @@
 package OOP;
 
 public class Account {
+    public String name;
+    public Account(String name){
+        this.name = name;
+    }
     static{
         System.out.println("init of the outer");
     }
     public static int x;
     public static int shared = 10;
     public static void main(String[] args) {
-        Account aaaaa = new Account();
+        Account aaaaa = new Account("hi");
         aaaaa.instanceMethod();
+        System.out.println(aaaaa.name);
     }
     public void mmmm(){
 
     }
     public void instanceMethod(){
-        Account a = new Account(){
+        Account a = new Account("HHH"){
             public void mmmm() {
                 System.out.println(Account.this);
                 System.out.println("mmmm method from the account new class");
+                System.out.println("name::::"+ name);
             }
         };
         a.mmmm();
-        Account aa = new Account();
+        Account aa = new Account("HHH");
         aa.mmmm();
     }
     public static void staticinstanceMethod(){
-        Account a = new Account(){
+        Account a = new Account("MMM"){
             public void mmmm() {
                 System.out.println();
                 System.out.println("mmmm method from the account new class");
             }
         };
         a.mmmm();
-        Account aa = new Account();
+        Account aa = new Account("MMM");
         aa.mmmm();
     }
 
--- Java-Basics/src/main/java/OOP/Day.java
+++ Java-Basics/src/main/java/OOP/Day.java
@@ -1,7 +1,7 @@
 package OOP;
 
 
-public class Day extends Account{
+public class Day {
 
     public static void main(String[] args) {
        
