cbc78c54ec7de4306286af670b40cb2cbe143a00
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-12-20 11:45:09 +0200

    practicing method references

--- Java-Basics/src/main/java/OOP/lambda/Demo.java
+++ Java-Basics/src/main/java/OOP/lambda/Demo.java
@@ -4,16 +4,27 @@ import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.lang.reflect.Method;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.lang.invoke.CallSite;
 import java.lang.invoke.ConstantCallSite;
 import java.lang.invoke.LambdaMetafactory;;
-public class Demo {
+public class Demo extends Demo2{
     private static final String HELLO = "Hello";
-    public static void main(String[] args) throws Exception {
-        int x = (int)Math.random();
-       Runnable r = () -> {};
-       Runnable rr = () -> System.out.println();     
-       System.out.println(r);
-       System.out.println(rr);   
-        }
+    public static void main(String[] args) {
+      Demo d = new Demo();
+      d.main();
+    }
+    public void main() {
+      Consumer<String> c = super::print; 
+    }
+    @Override
+    public void print(String s){
+      System.out.println(s);
+    }
+
+}
+class Demo2{
+  public void print(String s){}
 }
