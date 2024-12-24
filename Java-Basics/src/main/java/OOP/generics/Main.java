6b89b0bdcdddadd382edc5b30b907b6889127f32
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-12-24 11:30:56 +0200

    practicing generic methods

--- Java-Basics/src/main/java/OOP/generics/Main.java
+++ Java-Basics/src/main/java/OOP/generics/Main.java
@@ -3,19 +3,31 @@ package OOP.generics;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
-    private Object object;
-    public void setObject(Object object){
+public class Main<T> {
+    private T object;
+
+    public void setObject(T object) {
         this.object = object;
     }
-    public Object getObject(){
+
+    public T getObject() {
         return this.object;
     }
 
+    public void print(T item) {
+        System.out.println(item);
+    }
+
     public static void main(String[] args) {
-            Main m = new Main();
-            m.setObject("hi");
-            int x = (int) m.getObject();
+        printList(new ArrayList<String>());
+    }
+    public static <E> void printList(List<E> list){
+        for (E item : list){
+            System.out.println(item);
+        }
     }
 }
 
+interface C<C> {
+    void print(C item);
+}
