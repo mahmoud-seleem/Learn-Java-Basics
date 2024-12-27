60df97c816ed08ec08aed7ed96967be4938a1bd6
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-12-27 22:50:40 +0200

    exploring annotations

--- Java-Basics/src/main/java/OOP/annotations/Demo.java
+++ Java-Basics/src/main/java/OOP/annotations/Demo.java
@@ -1,11 +1,16 @@
 package OOP.annotations;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.annotation.Annotation;
 
-@Target(value = {ElementType.ANNOTATION_TYPE,ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
 public @interface Demo {
-    String value() default "";
+
+    int id();
 }
--- Java-Basics/src/main/java/OOP/annotations/Test.java
+++ Java-Basics/src/main/java/OOP/annotations/Test.java
@@ -1,9 +1,25 @@
 package OOP.annotations;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+import test.demo;
+
 public class Test {
 
-    @Demo
-    void testMethod(){
+    @Demo(id = 10)
+    void testMethod() {
         System.out.println("hi from test method");
+
+    }
+
+    public static void main(String[] args) {
+        Method[] methods = Test.class.getDeclaredMethods();
+        for (Method method : methods){
+            Annotation[] annotations = method.getDeclaredAnnotations();
+            for (Annotation a : annotations){
+                System.out.println(a.annotationType().getSimpleName());
+            }
+        }
     }
 }
