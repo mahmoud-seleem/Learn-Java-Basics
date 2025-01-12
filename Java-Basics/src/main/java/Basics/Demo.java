2c327a089d2def15c87f4ab598c99fc8c22462d3
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2025-01-12 13:09:08 +0200

    switch statement

--- Java-Basics/src/main/java/Basics/Demo.java
+++ Java-Basics/src/main/java/Basics/Demo.java
@@ -2,6 +2,16 @@ package Basics;
 
 public class Demo {
     public static void main(String[] args) {
-        /* */
+        int x = 0;
+        switch (x){
+            case 7:
+            System.err.println("sfasd");
+            case 8:
+            System.out.println("");
+            break;
+            default:
+            System.out.println("default");
+            break;
         }
+    }
 }
