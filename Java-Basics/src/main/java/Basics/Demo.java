08b174474bf63c32e1f03031824934da8f752959
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2025-01-12 20:46:50 +0200

    method overloading

--- Java-Basics/src/main/java/Basics/Demo.java
+++ Java-Basics/src/main/java/Basics/Demo.java
@@ -3,9 +3,13 @@ package Basics;
 import java.lang.invoke.StringConcatFactory;
 public class Demo {
     public static void main(String[] args) {
-        short x = print((short)5000001);
+        short x = print((short)5000000);
     }
     public static short print(short x ){
+        System.out.println(x);
+        return x;
+    }
+    public static char print(char x ){
         System.out.println(x);
         return x;
     }
