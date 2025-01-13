fc73750ca8ed7b3b0f7aa9412e8ee292b3fb8101
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2025-01-13 21:43:38 +0200

    multidimensional arrays and multinew array instruction

--- Java-Basics/src/main/java/Basics/Demo.java
+++ Java-Basics/src/main/java/Basics/Demo.java
@@ -3,14 +3,6 @@ package Basics;
 import java.lang.invoke.StringConcatFactory;
 public class Demo {
     public static void main(String[] args) {
-        short x = print((short)5000000);
-    }
-    public static short print(short x ){
-        System.out.println(x);
-        return x;
-    }
-    public static char print(char x ){
-        System.out.println(x);
-        return x;
-    }
+        int[][] x = new int[10][5];
+}
 }
