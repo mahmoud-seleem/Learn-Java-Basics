9e843709b95f7a17597be5cb9620a6ab316b3378
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2025-01-18 22:16:44 +0200

    we can mark a class as an abstract class without any need to have an abstract methods inside

--- Java-Basics/src/main/java/Basics/Demo.java
+++ Java-Basics/src/main/java/Basics/Demo.java
@@ -2,21 +2,9 @@ package Basics;
 
 import java.lang.invoke.StringConcatFactory;
 
-public class Demo {
+public abstract class  Demo {
     public static void main(String[] args) {
-        Object myObject = null;
-        // get object from somewhere
-
-        if (myObject instanceof String str) {
-            System.out.println(str.substring(0, 5));
-        }
-        // str = "dfs";
-        // istead of
-        if (myObject instanceof String) {
-            String str = (String) myObject;
-            System.out.println(str.substring(0, 5));
-        }
-
+    
     }
 
 }
