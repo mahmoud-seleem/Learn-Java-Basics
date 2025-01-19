68478da6655450d51f460ae8dc24de0999b9687c
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2025-01-19 19:35:49 +0200

    building a backend server that push notification to users via pushbullet apis

--- Java-Basics/src/main/java/Basics/Demo.java
+++ Java-Basics/src/main/java/Basics/Demo.java
@@ -2,34 +2,56 @@ package Basics;
 
 import java.io.IOException;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class Demo {
+    private static final String API_URL = "https://api.pushbullet.com/v2/pushes";
+    private static final String ACCESS_TOKEN = "o.mpnOQPzwiNgMIoomPeMMG99rFdnOm3Vg";
+
     public static void main(String[] args) {
-        String BASE_URL = "https://api.notion.com/v1/";
-        String TOKEN = "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";
-        OkHttpClient client = new OkHttpClient();
+        // String BASE_URL = "https://api.notion.com/v1/";
+        // String TOKEN = "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";
         // Example: Fetch a page
-        String pageId = "178da782c9e5806a8917cd45db2fc3c6";
+        // String pageId = "178da782c9e5806a8917cd45db2fc3c6";
+        String json = """
+                    {
+                      "type": "note",
+                      "title": "Hello from Pushbullet",
+                      "body": "This is a test notification sent via Java!"
+                    }
+                """;
+        RequestBody body = RequestBody.create(json, MediaType.get("application/json"));
+        OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url(BASE_URL + "users")
-                .addHeader("Authorization", "Bearer " + TOKEN)
-                .addHeader("Notion-Version", "2022-06-28")
+                .url(API_URL)
+                .post(body)
+                .addHeader("Access-Token", ACCESS_TOKEN)
+                .addHeader("Content-Type", "application/json")
                 .build();
 
-        try{
-            Response response = client.newCall(request).execute();
-        if (response.isSuccessful()) {
-            System.out.println(response.body().string());
-        } else {
-            System.err.println("Request failed: " + response.code());
-        }
-        }catch(IOException exception){
+        try {
+            for (int i = 0; i < 10; i++) {
+                Thread.sleep(2000);
+                Response response = client.newCall(request).execute();
+                if (response.isSuccessful()) {
+
+                    System.out.println(response.body().string());
+                } else {
+                    System.err.println("Request failed: " + response.code());
+                }
+            }
+        } catch (IOException exception) {
             System.out.println(exception);
-        }             
-        
+        }catch(InterruptedException exception){
+            System.out.println(exception);
+        }
+
     }
 
 }
