7080a08e5290f7532b4126be04ccc8d0b4b7ffb9
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2025-01-21 12:54:51 +0200

    accessing static field from the parent class

--- Java-Basics/src/main/java/OOP/Day.java
+++ Java-Basics/src/main/java/OOP/Day.java
@@ -1,10 +1,19 @@
 package OOP;
 
 
-public class Day {
+public class Day extends Praent {
 
     public static void main(String[] args) {
-       
+       System.out.println(Day.x);
+    }
+    Day(){
+        return;
     }
 
+}
+class Praent{
+    static{
+        System.out.println("initialization of the parent");
+    }
+    public static final int x = 10;
 }
