1963570eb5160407c4ee896dd24c347058d93974
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2025-01-21 17:19:00 +0200

    accessing shadowed fields inside the subclass

--- Java-Basics/src/main/java/OOP/Day.java
+++ Java-Basics/src/main/java/OOP/Day.java
@@ -2,16 +2,20 @@ package OOP;
 
 
 public class Day extends Praent {
-
+    public int y = 10  ;
+    public void m(){
+        System.out.println(y);
+        System.out.println(super.y);
+    }
     public static void main(String[] args) {
-       System.out.println(Day.x);
+        Day d = new Day();
+        d.m();
     }
-    Day(){
-        return;
-    }
+
 
 }
 class Praent{
+    public int y = 5;
     static{
         System.out.println("initialization of the parent");
     }
