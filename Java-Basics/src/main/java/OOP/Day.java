e585eadb0a56e99b263b436dbc7fd5bb2aa3d0d2
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2025-01-22 18:09:02 +0200

    polymerphism and it's typs

--- Java-Basics/src/main/java/OOP/Day.java
+++ Java-Basics/src/main/java/OOP/Day.java
@@ -4,14 +4,17 @@ package OOP;
 public class Day extends Praent implements Playable{
     public int y = 10  ;
     public void m(){
-       
         System.out.println(y);
         System.out.println(super.y);
             
     }
+    private  void pr(){
+        System.out.println("aaaaaafsf");
+    }
     public static void main(String[] args) {
-        Day d = new Day();
-        d.m();
+        PlayableParent2 d = new Day();
+        ///d.m();
+        d.printing();
     }
 
 
@@ -22,12 +25,26 @@ class Praent{
         System.out.println("initialization of the parent");
     }
     public static final int xx = 10;
+    public static void mm(){}
+    
 }
-interface Playable extends PlayableParent{
+interface Playable extends PlayableParent2{
     default int print(int x){
-        return 10 ;
+        return 5;
+    };
+}
+// interface PlayableParent{
+//     default int print(int x){
+//         return 0;
+//     }
+// }
+interface PlayableParent2{
+    int print(int x);
+    public default void printing(){
+        pr();
+    } 
+    private  void pr(){
+        System.out.println("afsf");
     }
 }
-interface PlayableParent{
-    int print(int x);
-}
+
