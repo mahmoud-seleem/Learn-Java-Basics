f6b0bbfb4c21fb2ab210db49dbecc6d8ae921677
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2025-01-24 23:49:13 +0200

    exception handling master

--- Java-Basics/src/main/java/OOP/exceptionHandling/Zero.java
+++ Java-Basics/src/main/java/OOP/exceptionHandling/Zero.java
@@ -1,11 +1,21 @@
 package OOP.exceptionHandling;
 
+import java.io.IOException;
 import java.util.InputMismatchException;
+import java.util.function.Predicate;
 
 public class Zero extends AutoClosing {
     public static void main(String[] args) throws Exception {
-        try (AutoClosing a = new AutoClosing()){
+        try (AutoClosing a = new AutoClosing()) {
             throw new ArithmeticException();
-        }
+        } catch ( ArithmeticException  e) {
+            System.out.println(e.getMessage());
+            Predicate xxxx = (z) -> {
+                if (z != null) return true ;
+                else {
+                    System.out.println(e);
+                    return false;}
+        };
     }
+}
 }
