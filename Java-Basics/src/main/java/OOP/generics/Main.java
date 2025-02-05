07a60186083310e8e6127eb070626096e68f434a
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2025-02-05 16:47:15 +0200

    revision on generics

--- Java-Basics/src/main/java/OOP/generics/Main.java
+++ Java-Basics/src/main/java/OOP/generics/Main.java
@@ -6,6 +6,32 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-       List<?> list = new ArrayList<Integer>();
+        List<String> list = new ArrayList<String>();
+        List<Integer> list2 = new ArrayList<>();
+        list.add("100");
+        List<?> list1 = list;
+        printList(list1);
+        list2.add(15);
+        printList2(list2);
+    }
+
+    public static void printList2(List<? extends Number > l){
+        for(Object o : l){
+            System.out.println(o);
+        }
+        Number n = l.get(0).doubleValue();
+        System.out.println(n);
+    }
+    public static <T> void printList(List<T> l) {
+        for (T t : l) {
+            System.out.println(t);
+        }
+        T tt = l.get(0);
+        System.out.println(tt);
+    }
+    public static <E> void g(E... a){
+        for (E e : a){
+            System.out.println(e);
+        }
     }
 }
